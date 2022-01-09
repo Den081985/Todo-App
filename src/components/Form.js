7346733ae1c5d812dc6f353af0d305ee44d47d0c
Den081985 <pishnenkod@gmail.com>
2022-01-09 19:54:20 +0300

    добавлены функции handleChange и handleSubmit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Form(props) {
+  //Используя хук  useState создаем начальное состояние и функцию-модификатор состояния setName
+  const [name, setName] = useState("");
+  //при изменении состояния ввода,вызывается setName
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+  //функция handleSubmit сробатывает при отправке формы
+  function handleSubmit(e) {
+    if (name === "") {
+      e.preventDefault();
+      //props.addTask("You need to fill input!");
+      setName("");
+    }
+    e.preventDefault();
+    props.addTask(name);
+    setName("");
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
         <label htmlFor="new-todo-input" className="label__lg">
           What needs to be done?
@@ -14,6 +31,8 @@ function Form(props) {
         className="input input__lg"
         name="text"
         autoComplete="off"
+        value={name}
+        onChange={handleChange}
       />
       <button type="submit" className="btn btn__primary btn__lg">
         Add
